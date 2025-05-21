a445c862834d03e6e7ad55e52080f15fb86a809a
dev0379 <dev.frontend.03@proton.me>
2025-05-21 21:09:57 +0545

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx'
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store.js"
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
       <Toaster position='top-center'/>
+      <Analytics/>
     </Provider>
   </StrictMode>,
 )
